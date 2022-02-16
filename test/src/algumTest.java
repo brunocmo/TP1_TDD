5416f51c00ef88e3349b14e89747af43e6b5a6e4
Bruno Nunes <brunocmo.nunes@gmail.com>
2022-02-16 19:59:45 -0300

    Initial Commit

---
+++ test/src/algumTest.java
@@ -0,0 +1,13 @@
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class algumTest {
+
+    @Test
+    public void testes() {
+        assertEquals(2, 2);
+
+    }
+
+}
