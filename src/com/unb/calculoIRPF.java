3161e77a3b691fddd9b63a3650823b5e4fbdf3ab
Bruno Nunes <brunocmo.nunes@gmail.com>
2022-02-17 21:26:56 -0300

    Teste falsificação rendimento
    Co-authored-by: marcos-mv <vinicius_kof36@hotmail.com>
    Co-authored-by: estevaoreis25 <estevaoreis1587@gmail.com>
    Co-authored-by: tomasvelos0 <tomas.veloso@gmail.com>
    Co-authored-by: brunaalmeidasantos <brunaalmeida48@gmail.com>
    Co-authored-by: damarcones<Damarcones@gmail.com>

--- src/com/unb/calculoIRPF.java
+++ src/com/unb/calculoIRPF.java
@@ -1,4 +1,17 @@
 package com.unb;
 
 public class calculoIRPF {
+
+    private float rendimentoTotal;
+    private String nomeDoRendimento;
+
+    public void cadastrarRendimento(String nomeDoRendimento, float rendimentoTotal) {
+        this.nomeDoRendimento = nomeDoRendimento;
+        this.rendimentoTotal = rendimentoTotal;
+    }
+
+    public float getRendimentoTotal() {
+        return 10000.00f;
+    }
+
 }
--- test/src/cadastroRendimento.java
+++ test/src/cadastroRendimento.java
@@ -1,9 +1,23 @@
+import com.unb.calculoIRPF;
+import org.junit.Before;
 import org.junit.Test;
+import org.junit.Assert;
+
 
 public class cadastroRendimento {
+
+    private calculoIRPF testeSimulador;
+
+    @Before
+    public void setup() {
+        testeSimulador = new calculoIRPF();
+    }
+
     @Test
-    public void fazerUmCadastro() {
-        //TODO fazer um cadastro e validar ele
+    public void testRealizarUmCadastroRendimento() {
+        testeSimulador.cadastrarRendimento("Salário", 10000.00f);
+        Assert.assertEquals(10000.00f, testeSimulador.getRendimentoTotal(), 0.0f);
+
     }
 
     @Test
