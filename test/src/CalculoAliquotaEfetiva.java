1b4aba591c2f8d23a9097aff1edff89d9d92db9d
Bruno Nunes <brunocmo.nunes@gmail.com>
2022-02-24 01:13:42 -0300

    Teste triangulação aliquota efetiva
    Co-authored-by: marcos-mv <vinicius_kof36@hotmail.com>
    Co-authored-by: estevaoreis25 <estevaoreis1587@gmail.com>
    Co-authored-by: tomasvelos0 <tomas.veloso@gmail.com>
    Co-authored-by: brunaalmeidasantos <brunaalmeida48@gmail.com>
    Co-authored-by: damarcones <damarcones@gmail.com>

--- test/src/CalculoAliquotaEfetiva.java
+++ test/src/CalculoAliquotaEfetiva.java
@@ -3,41 +3,91 @@ import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.experimental.categories.Category;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
 
+import java.util.Arrays;
+import java.util.Collection;
+
+@RunWith(Parameterized.class)
 public class CalculoAliquotaEfetiva {
 
     private CalculoIRPF simulador;
+
+    Object[][] registros;
+    float valorEsperado;
 
     @Before
     public void setup(){
         simulador = new CalculoIRPF();
     }
 
-    @Test
-    @Category(TesteFuncional.class)
-    public void calcAliquotaEfetivaValorAlto() {
-        simulador.cadastrarRendimento("Sálario", 10000f);
-        simulador.cadastrarPrevidenciaOficial("Contribuicao Compulsoria", 800f);
-        simulador.cadastrarDependentes("Julio Cesar", "17/07/2017");
-        simulador.cadastrarPensaoAlimenticia(2000f);
-        simulador.cadastrarOutrasDeducoes("Previdência Privada", 1000f);
-        simulador.calcularImposto();
+    public CalculoAliquotaEfetiva(Object[][] registros, float valorEsperado) {
+        this.registros = registros;
+        this.valorEsperado = valorEsperado;
+    }
 
-        Assert.assertEquals( 0.0783f, simulador.getCalculoAliquotaEfetiva(), 0.0001f );
+    @Parameterized.Parameters
+    public static Collection<Object[]> getParameters() {
+        Object[][] parametros = new Object[][]{
+                {new Object[][]{
+                        {
+                                "Salario", 3500f,
+                                "Contribuicao Compulsoria", 1000f,
+                                "Pedrinho", "03/08/2019",
+                                500f,
+                                "FAPI", 300f
+                        },
+                }, 0.0f },
+                {new Object[][]{
+                        {
+                                "Salario", 6300f,
+                                "Outra contribuicao oficial", 400f,
+                                "Joaozinho", "13/02/2021",
+                                800f,
+                                "Funpresp", 500f
+                        },
+                }, 0.0565f },
+                {new Object[][]{
+                        {
+                                "Salario", 4300f,
+                                "Previdencia Oficial", 400f,
+                                "", "",
+                                0f,
+                                "Previdencia Privada", 600f
+                        },
+                }, 0.0326f },
+                {new Object[][]{
+                        {
+                                "Salario", 12000f,
+                                "Contribuicao Compulsoria", 1000f,
+                                "Pedrinho", "03/08/2019",
+                                500f,
+                                "FAPI", 300f
+                        },
+                        {
+                                "Aluguel", 3000f,
+                                "Outra contribuicao oficial", 500f,
+                                "Joaozinho", "13/02/2021",
+                                800f,
+                                "Funpresp", 500f
+                        },
+                }, 0.1440f },
+        };
+        return Arrays.asList(parametros);
     }
 
     @Test
     @Category(TesteFuncional.class)
-    public void calcAliquotaEfetivaValorBaixo() {
-        simulador.cadastrarRendimento("Alugel", 3200f);
-        simulador.cadastrarPrevidenciaOficial("Previdencia oficial", 400f);
-        simulador.cadastrarDependentes("Ricardo", "13/02/2021");
-        simulador.cadastrarDependentes("Letícia", "02/08/2017");
-        simulador.cadastrarDependentes("João", "30/04/2016");
-        simulador.cadastrarPensaoAlimenticia(0f);
-        simulador.cadastrarOutrasDeducoes("FAPI", 200f);
-        simulador.calcularImposto();
-
-        Assert.assertEquals( 0.0029f, simulador.getCalculoAliquotaEfetiva(), 0.0001f );
+    public void calcAliquotaEfetiva() {
+        for (Object[] registro : registros){
+            simulador.cadastrarRendimento(((String)registro[0]), (float)registro[1]);
+            simulador.cadastrarPrevidenciaOficial(((String)registro[2]), (float)registro[3]);
+            simulador.cadastrarDependentes((String)registro[4], (String)registro[5]);
+            simulador.cadastrarPensaoAlimenticia((float)registro[6]);
+            simulador.cadastrarOutrasDeducoes((String)registro[7], (float)registro[8]);
+            simulador.calcularImposto();
+        }
+        Assert.assertEquals( valorEsperado, simulador.getCalculoAliquotaEfetiva(), 0.05f);
     }
 }
